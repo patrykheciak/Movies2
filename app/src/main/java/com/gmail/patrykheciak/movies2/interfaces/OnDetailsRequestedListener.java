d7358b0a089ebfdbda7a82e417a16b0400cc2d58
patrykheciak <patrykheciak@gmail.com>
2017-05-23 22:06:20 +0200

    deleted unnecessary ListFragment and OnDetailsRequestedListener

--- app/src/main/java/com/gmail/patrykheciak/movies2/interfaces/OnDetailsRequestedListener.java
+++
@@ -1,7 +0,0 @@
-package com.gmail.patrykheciak.movies2.interfaces;
-
-import com.gmail.patrykheciak.movies2.entities.Movie;
-
-public interface OnDetailsRequestedListener {
-    void onDetailsRequested(Movie movie, int position);
-}
